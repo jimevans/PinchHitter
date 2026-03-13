c8e9ea040c405dba16c6ed18c11ab5556d52e4c9
Jim Evans <james.h.evans.jr@gmail.com>
2026-03-13 14:22:32 -0400

    fix: update client to properly dispose of server

--- src/PinchHitter.Client/Program.cs
+++ src/PinchHitter.Client/Program.cs
@@ -2,7 +2,7 @@
 
 // See https://aka.ms/new-console-template for more information
 
-Server server = new();
+await using Server server = new();
 
 server.RegisterHandler("/", new RedirectRequestHandler("/index.html"));
 server.RegisterHandler("/index.html", new WebResourceRequestHandler(WebContent.AsHtmlDocument("<h1>Welcome to the PinchHitter web server</h1><p>You can browse using localhost</p>")));
@@ -17,4 +17,4 @@ server.Start();
 Console.WriteLine($"Serving pages at http://localhost:{server.Port}.");
 Console.WriteLine("Press <Enter> to shut down the server.");
 Console.ReadLine();
-server.Stop();
+await server.StopAsync();
